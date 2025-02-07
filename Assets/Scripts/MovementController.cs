1acdcb88d4d4cb6098eb96e3614cd1f5e6f93511
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-02-07 02:28:50 -0800

    Added dash drag and ground detection.

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,9 +7,19 @@ public class MovementController : MonoBehaviour
 
     float xInput;
     float yInput;
-    [SerializeField] float moveSpeed;
+    float moveSpeed;
+
+    [SerializeField] float groundspeed;
+    [SerializeField] float airSpeed;
     [SerializeField] float mouseSpeed;
     [SerializeField] float dashSpeed;
+    [SerializeField] float dragDelay;
+
+    [Header("Ground Check")]
+    [SerializeField] float groundDrag;
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask groundLayer;
+    bool isGrounded;
 
     Rigidbody rb;
 
@@ -32,9 +42,21 @@ public class MovementController : MonoBehaviour
         //Vector3 mouseInput = new Vector3(0, Input.GetAxis("Mouse X"), 0);
         //transform.eulerAngles += mouseInput * mouseSpeed;
 
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundLayer / 2 + 0.2f, groundLayer);
+
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+            moveSpeed = groundspeed;
+        }
+        else
+        {
+            moveSpeed = airSpeed;
+        }
+
         if(Input.GetMouseButtonDown(0))
         {
-            rb.AddForce(cameraHold.transform.forward * dashSpeed, ForceMode.Impulse);
+            StartCoroutine(HandleDash());
         }
     }
 
@@ -47,5 +69,15 @@ public class MovementController : MonoBehaviour
         playerInput.y = 0;
 
         rb.AddForce (playerInput * moveSpeed, ForceMode.Force);
+    }
+
+    IEnumerator HandleDash()
+    {
+        rb.drag = groundDrag;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(cameraHold.transform.forward * dashSpeed, ForceMode.Impulse);
+        yield return new WaitForSeconds(dragDelay);
+        rb.drag = 0;
+
     }
 }
