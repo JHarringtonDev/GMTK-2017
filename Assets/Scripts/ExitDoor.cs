6bfa684548d253c383f3cae39c099578e1b1db26
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-02-08 00:08:30 -0800

    Added exit door animation

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class ExitDoor : MonoBehaviour
 {
     [SerializeField] int runesToActivate;
+    [SerializeField] float doorSpeed;
+    [SerializeField] GameObject leftDoor;
+    [SerializeField] GameObject rightDoor;
+    [SerializeField] MeshCollider exitPlane;
+    float doorRotation = 0;
+    bool opening;
+
+    private void Update()
+    {
+        if (opening && doorRotation < 100)
+        {
+            doorRotation += doorSpeed * Time.deltaTime;
+        }
+
+        leftDoor.transform.localEulerAngles = new Vector3 (0, doorRotation, 0);
+        rightDoor.transform.localEulerAngles = new Vector3 (0,-doorRotation, 0);
+    }
 
     void OpenDoor()
     {
         Debug.Log("Open Door");
+        opening = true;
+        exitPlane.enabled = true;
     }
 
     public void ActivateRune() 
