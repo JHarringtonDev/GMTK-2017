5d08913b0dafb089832a9b82139f87040c29244e
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-02-08 00:42:55 -0800

    Added fade at level start and scene transition

--- Assets/Scripts/ExitPlane.cs
+++ Assets/Scripts/ExitPlane.cs
@@ -6,11 +6,26 @@ using UnityEngine.SceneManagement;
 public class ExitPlane : MonoBehaviour
 {
     [SerializeField] string nextLevel;
+    [SerializeField] float nextLevelTime;
+    FadeScript fadeScript;
+
+    private void Start()
+    {
+        fadeScript = FindObjectOfType<FadeScript>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<MovementController>() != null)
         {
-            SceneManager.LoadScene(nextLevel);
+            StartCoroutine(BeginNextLevel());
         }
+    }
+
+    IEnumerator BeginNextLevel()
+    {
+        fadeScript.FadeOut();
+        yield return new WaitForSeconds(nextLevelTime);
+        SceneManager.LoadScene(nextLevel);
     }
 }
---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    [SerializeField] Image fadeImage;
+    [SerializeField] float fadeSpeed;
+    float alphaValue;
+    bool fadingIn;
+    bool fadingOut;
+
+    private void Start()
+    {
+        FadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fadingIn)
+        {
+            if(fadeImage.color.a > 0)
+            {
+                alphaValue -= fadeSpeed * Time.deltaTime;
+                fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, alphaValue);
+            }
+            else
+            {
+                fadingIn = false;
+            }
+        } 
+        else if (fadingOut)
+        {
+            if (fadeImage.color.a < 1)
+            {
+                alphaValue += fadeSpeed * Time.deltaTime;
+                fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, alphaValue);
+            }
+            else
+            {
+                fadingOut = false;
+            }
+        }
+    }
+
+    void FadeIn()
+    {
+        alphaValue = 1;
+        fadingIn = true;
+    }
+
+    public void FadeOut()
+    {
+        fadingOut = true;
+    }
+}
