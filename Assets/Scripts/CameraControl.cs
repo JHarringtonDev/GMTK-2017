b7e5e588ce181c7ed459200a0ee5b7cf4a7854b1
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-02-08 21:45:26 -0800

    Adjusted camera control for clamped x axis

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,11 +6,26 @@ public class CameraControl : MonoBehaviour
 {
     [SerializeField] float mouseSpeed;
 
+    float yRotation;
+    float xRotation;
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 mouseInput = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
-        transform.eulerAngles += mouseInput * mouseSpeed;
+        //Vector3 mouseInput = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * mouseSpeed;
+        //
+        //float xRotation = transform.eulerAngles.x + mouseInput.x;
+        //
+        //float clampedX = Mathf.Clamp(xRotation, -30f, 30f);
+        //transform.eulerAngles = new Vector3(clampedX, transform.localEulerAngles.y + mouseInput.y, 0);
+
+        yRotation -= mouseSpeed * Input.GetAxis("Mouse Y");
+        xRotation = mouseSpeed * Input.GetAxis("Mouse X");
+
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(yRotation, transform.localEulerAngles.y + xRotation, 0);
+
     }
 
     //Input.GetAxis("Mouse Y")
